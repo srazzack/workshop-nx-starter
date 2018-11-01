a05135fd67f1d06460c94b8132d27458c7e14d79
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-11-01 11:46:42 -0500

    (ngrx): Lab 7 - Use NgRx Facade

--- libs/ticket-list-view/src/lib/ticket-details/ticket-details.component.ts
+++ libs/ticket-list-view/src/lib/ticket-details/ticket-details.component.ts
@@ -4,11 +4,10 @@ import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 
 import { map } from 'rxjs/operators';
-import { select, Store } from '@ngrx/store';
 
-import { Ticket, TicketComment } from '@tuskdesk-suite/data-models';
-import { LoadTicket, PartialAppState, ticketsQuery } from '@tuskdesk-suite/tickets-state';
+import { TicketsFacade } from '@tuskdesk-suite/tickets-state';
 import { TicketTimerService } from '../ticket-timer.service';
+import { Ticket, TicketComment } from '@tuskdesk-suite/data-models';
 
 @Component({
   selector: 'app-ticket-details',
@@ -23,7 +22,7 @@ export class TicketDetailsComponent implements OnInit {
   markedToWork$: Observable<boolean>;
 
   constructor(
-    private store: Store<PartialAppState>,
+    private facade: TicketsFacade,
     private route: ActivatedRoute,
     private ticketTimerService: TicketTimerService
   ) {}
@@ -31,13 +30,17 @@ export class TicketDetailsComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(params => {
       const id = +params['id'];
+      this.ticket$ = this.facade.entities$.pipe(
+        map(tickets => {
+          return tickets[id];
+        })
+      );
+
       this.markedToWork$ = this.ticketTimerService.ticketsToWork$.pipe(
         map(tickets => {
           return tickets.indexOf(id) !== -1;
         })
       );
-
-      this.ticket$ = this.store.pipe(select(ticketsQuery.getTicketAsEntities), map(tickets => tickets[id]));
     });
   }
 
